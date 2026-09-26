81cd8ff03ae1a82c4954b631d599fb7f41eaa67d
aishwarya <ashp17star@gmail.com>
2026-09-26 17:00:46 +0530

    Metrics calculation for k-means clustering for ML algorithm

--- ml_basics_3.py
+++ ml_basics_3.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
 iris_dataset = load_iris()
 print("-----------Iris dataset:-------------")
@@ -16,6 +17,12 @@ print(iris_data.shape)
 
 model = KMeans(n_clusters=3,random_state=42)
 model.fit(iris_data)
+
+print("Model labels : ", model.labels_)
+
+#silhouette score ranges between -1 to +1
+s_score = silhouette_score(iris_data,model.labels_)
+print("Silhouette score for iris dataset:", s_score)
 
 plt.scatter(iris_data[0:150,2], iris_data[0:150,3], c = model.labels_)
 plt.title("K-means clustering for iris dataset")
